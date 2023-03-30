4ae553f4fa60224be79463d8e31896e1ee5ab09c
Woojin Lee <128646023+woojin-lee-ucla@users.noreply.github.com>
2023-03-29 22:31:30 -0700

    Add files via upload

---
+++ 4 transfer .gjf .py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+# create new directory for .gjf files
+if not os.path.exists('conformers'):
+    os.makedirs('conformers')
+
+# move all .gjf files to the new directory
+for file in os.listdir('.'):
+    if file.endswith('.gjf'):
+        shutil.move(file, 'conformers/' + file)

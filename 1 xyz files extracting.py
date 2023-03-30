d0c2748623248ce57c0680ec9ddb91c784f193e1
Woojin Lee <128646023+woojin-lee-ucla@users.noreply.github.com>
2023-03-29 22:28:10 -0700

    Update 1 xyz files extracting.py

--- 1 xyz files extracting.py
+++ 1 xyz files extracting.py
@@ -8,6 +8,7 @@ with open('crest_conformers.xyz', 'r') as f:
 for i in range(100):
     file_name = f'file_{i}.xyz'
     file_data = data[i*50:(i+1)*50] # lines for each .xyz format for example between 1.xyz and 2.xyz
+    # here this example has 50 lines for each coordinate of 1 .xyz
     
     # write the data to a new file
     with open(file_name, 'w') as f:
@@ -19,7 +20,3 @@ for i in range(100):
 
 
 
-# 1 xyz files extracting.py # You can extract each .xyz coordinate from merged .xyz file (crest_conformers.xyz)
-# 2 preparing Gaussian input files.py # you need to install ase through 'pip install ase' then, enter below in your terminal 
-# for i in {0..27}; do ase-gui file_"$i".xyz -o file_"$i".gjf; done
-# 3 add input files automatically in each .gjf

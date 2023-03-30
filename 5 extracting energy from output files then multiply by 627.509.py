4aba3b0415ff0e93d0ea8987137527f68e223068
Woojin Lee <128646023+woojin-lee-ucla@users.noreply.github.com>
2023-03-29 22:37:14 -0700

    Update 5 extracting energy from output files then multiply by 627.509.py

--- 5 extracting energy from output files then multiply by 627.509.py
+++ 5 extracting energy from output files then multiply by 627.509.py
@@ -1,3 +1,5 @@
+# After you sumit the jobs in the cluster with .gjf files, the .out files will be generated. This code automatically finds electronic energy values and multiply by 627.509 to convert the value in the unit of kcal/mol
+# The code will show the values of kcal/mol. You can copy and paste to the excel sheet, which can make a column of the energies. Then, you can easily compare the energies of conformers with the excel sheet.
 import re
 
 # Define the regular expression pattern to search for the values after "HF=", excluding "0.,"

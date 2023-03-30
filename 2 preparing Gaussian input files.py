aae0cc17bfac643b244b9f1ec8e146457acf5b11
Woojin Lee <128646023+woojin-lee-ucla@users.noreply.github.com>
2023-03-29 22:29:03 -0700

    Update 2 preparing Gaussian input files.py

--- 2 preparing Gaussian input files.py
+++ 2 preparing Gaussian input files.py
@@ -1,3 +1,5 @@
 pip install ase
 
-1 extracting_xyz_files.py
+for i in {0..100}; do ase-gui file_"$i".xyz -o file_"$i".gjf; done
+
+# now you install ase, .xyz can be automatically reformed into .gjf. This is Shell script.
